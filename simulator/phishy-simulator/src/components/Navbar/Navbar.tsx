846dd296491f43a00f72aab6f3d66726b88c57a9
Eliza Lee <elizamlee5@gmail.com>
2023-10-11 20:23:36 +1100

    Navbar + basic styling

--- simulator/phishy-simulator/src/components/Navbar/Navbar.tsx
+++ simulator/phishy-simulator/src/components/Navbar/Navbar.tsx
@@ -2,7 +2,41 @@ import React from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <a href="#" className="logo">
+        phishy
+      </a>
+
+      <ul className="nav-menu">
+        <li>
+          <a className="nav-button" href="#">
+            Home
+          </a>
+        </li>
+        <li>
+          <a className="nav-button" href="#">
+            Simulations
+          </a>
+        </li>
+        <li>
+          <a className="nav-button" href="#">
+            Learn
+          </a>
+        </li>
+        <li>
+          <a className="nav-button" href="#">
+            Documentation
+          </a>
+        </li>
+        <li>
+          <a className="nav-button" href="#">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
