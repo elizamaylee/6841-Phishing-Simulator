f35de7cbcd342de2cf11f832c68fb596824a9d8f
Eliza Lee <elizamlee5@gmail.com>
2023-10-11 20:24:07 +1100

    Import images

---
+++ simulator/phishy-simulator/src/assets/index.tsx
@@ -0,0 +1,6 @@
+import arc from "./arc.png";
+import logo from "./phishy-logo.svg";
+import referendum from "./referendum.png";
+import unsw from "./unsw.png";
+
+export { arc, logo, referendum, unsw };
