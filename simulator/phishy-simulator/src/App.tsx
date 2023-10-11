f8c9798aef6f4da4c5a49d87ba2517b3237384ac
Eliza Lee <elizamlee5@gmail.com>
2023-10-12 08:24:15 +1100

    Template cards and general styling

--- simulator/phishy-simulator/src/App.tsx
+++ simulator/phishy-simulator/src/App.tsx
@@ -1,11 +1,33 @@
 import React from "react";
 import { Navbar, TemplateCard } from "./components";
+import "./App.css";
 
 const App = () => {
   return (
     <>
       <Navbar></Navbar>
-      <TemplateCard></TemplateCard>
+      <div className="body-container">
+        <div className="content-container">
+          <div className="heading-container">
+            <h1>Select a phishing template</h1>
+            <div className="description">
+              This template will be used to create the email users will see when
+              the simulation begins.
+            </div>
+          </div>
+          <div className="cards-gallery">
+            <TemplateCard
+              title="Important Arc Club Executive Updates"
+              active
+            ></TemplateCard>
+            <TemplateCard
+              title="UNSW Exam Timetable Release"
+              active
+            ></TemplateCard>
+            <TemplateCard title="2023 Referendum" active></TemplateCard>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- simulator/phishy-simulator/src/components/TemplateCard/TemplateCard.tsx
+++ simulator/phishy-simulator/src/components/TemplateCard/TemplateCard.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./TemplateCard.css";
+import { arc, unsw, referendum } from "../../assets";
+import Label from "../Label/Label";
 
-const TemplateCard = () => {
-  return <div></div>;
+const TemplateCard = (props: { title: string; active: boolean }) => {
+  const { title, active } = props;
+  return (
+    <template>
+      <div className="top-rectangle"></div>
+      <div className="card-content-container">
+        <img className="template-image" src={arc} alt="" />
+        <div className="template-title">{title}</div>
+        <Label description="university students" colour="blue"></Label>
+      </div>
+    </template>
+  );
 };
 
 export default TemplateCard;
