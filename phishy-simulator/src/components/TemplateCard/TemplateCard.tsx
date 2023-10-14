bc0908a5355ab02eb847aa6b7e1ae84c1d9de4a6
Eliza Lee <elizamlee5@gmail.com>
2023-10-14 13:21:18 +1100

    Map respective image to template card and fix image fit

--- phishy-simulator/src/components/TemplateCard/TemplateCard.tsx
+++ phishy-simulator/src/components/TemplateCard/TemplateCard.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import "./TemplateCard.css";
-import { arc, unsw, referendum } from "../../assets";
 import Label from "../Label/Label";
 
 const TemplateCard = (props: {
   title: string;
   onClick: React.MouseEventHandler<HTMLTemplateElement>;
+  img: string;
 }) => {
-  const { title, onClick } = props;
+  const { title, onClick, img } = props;
   return (
     <template onClick={onClick}>
       <div className="top-rectangle"></div>
       <div className="card-content-container">
-        <img className="template-image" src={arc} alt="" />
+        <img className="template-image" src={img} alt="" />
         <div className="template-title">{title}</div>
         <Label description="university students" colour="blue"></Label>
       </div>
